c836504e94ef4c220316f974fd8c5a55e3e29ab5
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-03 20:07:49 +0000

    Add basic router in App.js, include link to Projects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Projects } from './containers';
 import './App.css';
+
+const api = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000'
+  : 'https://http://abwi.herokuapp.com';
+
+let Home = (props) => (<h1>Home</h1>);
+
+let buildProjects = function() { 
+  return <Projects fetch={fetch.bind(this)} api={api} /> 
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={buildProjects} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
-}
+};
 
 export default App;
+export { api };
