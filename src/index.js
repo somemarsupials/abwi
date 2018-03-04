a7b85b9b0c5cf58617d22f44be50ded4e919ddcf
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-04 15:26:50 +0000

    Include redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';                                                                           
+import { Provider } from 'react-redux';                                                                        
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const store = createStore(reducer);                                                                         
+window.store = store;
+                                                                                                               
+ReactDOM.render(                                                                                               
+  <Provider store={store}>                                                                                     
+    <App />                                                                                                    
+  </Provider>,                                                                                                 
+  document.getElementById('root')                                                                              
+);
+
 registerServiceWorker();
