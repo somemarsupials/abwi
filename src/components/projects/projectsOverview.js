664a9df24b1662b4776c86ac984a569bae6190d4
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-08 16:24:10 +0000

    Add project fetching and error logic to ProjectList

--- src/components/projects/projectsOverview.js
+++ src/components/projects/projectsOverview.js
@@ -4,24 +4,20 @@ import { TitleBar, ErrorBar, InfoBar } from '../common/';
 import ProjectList from './projectList';
 
 export default function(props) {
-  console.log(props);
-  if (props.error || !props.projects) {
-    return (<ErrorBar 
-        text={'Could not find projects'}
-        detail={props.error}
-    />);
-  };
-
-  if (props.fetching) {
-    return (<InfoBar text={'Fetching projects...'} />);
-  };
+  let error = props.error || !props.projects;
 
   return (
     <div>
       <TitleBar text={'Projects'} /> 
-      <ProjectList 
-        projects={props.projects} 
-      />
+      { error &&
+        <ErrorBar text={'Could not find projects'} detail={props.error} />
+      }
+      { !error && props.fetching && 
+        <InfoBar text={'Fetching projects...'} />
+      }
+      { !error && !props.fetching &&
+        <ProjectList projects={props.projects} />
+      }
     </div>
   );
 };
