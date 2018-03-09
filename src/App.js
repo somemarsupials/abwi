5f2f823918927279b206a4529f421a7919580875
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-09 15:12:55 +0000

    Use new container names, add projects/:id route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { Header } from './components/header';
-import { ProjectsContainer } from './containers';
+import { ProjectsContainer, ProjectContainer } from './containers';
 import './App.css';
 
 let api;
@@ -20,6 +20,7 @@ class App extends Component {
           <Header />
           <div className="App">
             <Route exact path="/projects" component={ProjectsContainer} />
+            <Route exact path="/projects/:id" component={ProjectContainer} />
           </div>
         </div>
       </BrowserRouter>
