a01802472082d721ebfc4f765453246af88bee7a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-10 18:29:37 +0000

    Remove redundant Link import from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Header } from './components/header';
 import { 
